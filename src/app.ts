e0865ea10bdc5b051e980c823e2faffda24b207d
Aelithron <aelithron@gmail.com>
2025-02-28 18:35:39 +0000

    a

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const app: Express = express();
+const port = process.env.PORT || 6677;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.contentType("application/json");
+  res.json({
+    server: "Static File Server",
+    accessing_on: req.hostname,
+    version: "DEV (not implemented)",
+    github_repo: "https://github.com/(placeholder)",
+    auth_valid: (req.headers['authorization'] === `Bearer ${process.env.API_KEY}`),
+  });
+});
+
+app.get("/healthz", (req: Request, res: Response) => {
+  res.contentType("application/json");
+  res.json({
+  });
+});
+
+if (process.env.NODE_ENV !== "test") { startServer(); }
+
+export function startServer() {
+  return app.listen(port, () => {
+    console.log(`[server] Server is running at http://localhost:${port}`);
+  });
+}
